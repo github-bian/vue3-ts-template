841d0365e78441ae10072a778bef4463f619462a
bian <bian@biandeMacBook-Pro.local>
2023-08-21 22:07:37 +0800

    💄 style:(custom): fix:修改文件

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,10 +1,10 @@
 <template>
 	<h2>这里是登录页面</h2>
 	<br />
-	<router-link to="/">点击跳转至首页</router-link>
+	<router-link to="/">跳转至首页</router-link>
 	<br /><br />
 
-	<router-link to="/vueUse">点击跳转至vueUse页面</router-link>
+	<router-link to="/vueUse">跳转至vueUse页面</router-link>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
