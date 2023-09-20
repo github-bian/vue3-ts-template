22e8015a28b6ff002a91242ecf0f3c0ffe143986
bian <bian@biandeMacBook-Pro.local>
2023-09-20 17:11:31 +0800

    fix 测试lint校验

--- src/views/index.vue
+++ src/views/index.vue
@@ -11,6 +11,11 @@ function request(num: number) {
 	});
 }
 
+
+
+
+
+
 async function getInfo() {
 	const sendData = {
 		username: 'bian',
@@ -26,7 +31,8 @@ async function getInfo() {
 	<div class="w-100 h-100 flex flex-col justify-around items-center rounded bg-blue-100 m-10 p-20">
 		<p class="text-5xl">Vite+Vue3+Ts</p>
 		<button class="btn" @click="getInfo">获取接口数据</button>
-		<button class="btn bg-green-400 rounded-md transform hover:bg-red-500 hover:scale-120 hover:rounded-full duration-1000">按钮1</button>
+		<button
+			class="btn bg-green-400 rounded-md transform hover:bg-red-500 hover:scale-120 hover:rounded-full duration-1000">按钮1</button>
 		<div>
 			<img class="w-20 h-20" alt="vite" src="@/assets/vite.png" />
 		</div>
